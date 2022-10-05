42fc9f480bbd4fcd9cf61183a58ebe711a0ef964
Shaqvier <88389639+Shaqvier@users.noreply.github.com>
2022-10-05 22:28:34 +0700

    Update ddf_write.ino

--- ddf_write/ddf_write.ino
+++ ddf_write/ddf_write.ino
@@ -1,51 +1,40 @@
 #include <EEPROM.h>
-#define cells 9
-#define rows 3
-#define columns 3
+int incomingByte = 1;  // for incoming Serial2 data
+int pseudoEEPROM[9];
+int newEEPROM[9];
+int i = 0;
+boolean done = false;
 
-float pseudoEEPROM[cells], val[rows][columns];
-int i = 0, j = 0, k = 0, l = 0;
-
-struct table_cell{
-  float throttle;
-  float rpm;
-  float injection;
-} table_cell;
-
-union table_in_ram{
-  byte raw[sizeof(struct table_cell) * cells];
-  struct table_cell parsed[cells];
-} table_in_ram;
-
+HardwareSerial Serial2(PA3, PA2);
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-
-  //baca EEPROM
-  for (i = 0; i < rows; i++) {
-    for (j = 0; j < columns; j++) {
-        EEPROM.get(k * sizeof(float), val[i][j]);
-        Serial.println(val[i][j]);
-        k++;
-    }
-  }
-
-  // EEPROM.clear();
+  Serial2.begin(9600);  // opens Serial2 port, sets data rate to 9600 bps
+  i = 0;
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  //coba pake pseudo EEPROM (array)
-  // if (Serial.available() && i < cells) {
-  //   pseudoEEPROM[i] = Serial.parseFloat();
-  //   Serial.println(pseudoEEPROM[i]);
-  //   i++;
-  // }
-
-  //coba update EEPROM beneran
-  if (Serial.available() && l < cells) {
-   EEPROM.put(l * sizeof(float), Serial.parseFloat());
-   l++;
+  // send data only when you receive data:
+  while (i < 9) {
+    if (Serial2.available() > 0) {
+      pseudoEEPROM[i] = Serial2.parseInt();
+      //EEPROM.write(i, pseudoEEPROM[i]);
+      Serial2.print("serial :");
+      Serial2.println(pseudoEEPROM[i]);
+      i++;
+    }
+  }
+  if (done == false) {
+    for (int r = 0; r < 9; r++) {
+      EEPROM.write(r, pseudoEEPROM[r]);
+      delay(100);
+    }
+    for (int x = 0; x < 9; x++) {
+      newEEPROM[x] = EEPROM.read(x);
+      Serial2.print("EEPROM :");
+      Serial2.println(newEEPROM[x]);
+      delay(100);
+    }
+    digitalWrite(LED_BUILTIN, HIGH);
+    done = true;
   }
 }
